f0ffb6b9b0cac3fe751feb565f0850782cf51ad6
Jai Rajput <jai@tecorb.co>
2019-11-29 18:38:32 +0530

    Strong Parameters: params permited while creating records

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,10 +1,19 @@
 class Api::V1::RegistrationsController < Api::V1::ApplicationController
 	def create
-		user = User.new(fname: params[:fname], lname: params[:lname], email: params[:email])
-		if user.save
-			render json: {code: 200, message: "Signup successful", data: user}
-		else
-			render json: {code: 401, message: user.errors.full_messages.join(", ")}
-		end
+		begin
+			user = User.new(params_permit)
+			if user.save
+				render json: {code: 200, message: "Signup successful", data: user}
+			else
+				render json: {code: 401, message: user.errors.full_messages.join(", ")}
+			end
+		rescue Exception => e
+			render json: {code: 402, message: e}
+		end	
+	end
+
+	private
+	def params_permit
+		params.require(:user).permit(:fname, :lname, :email)	
 	end
 end
