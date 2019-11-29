7179da68ad42b98eeed64dc9d4ddade938cabc3e
Jai Rajput <jai@tecorb.co>
2019-11-29 18:32:12 +0530

    Model validations

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::RegistrationsController < Api::V1::ApplicationController
 	def create
-		user = User.create(fname: params[:fname], lname: params[:lname], email: params[:email])
-		render json: {result: @result, data: user}
+		user = User.new(fname: params[:fname], lname: params[:lname], email: params[:email])
+		if user.save
+			render json: {code: 200, message: "Signup successful", data: user}
+		else
+			render json: {code: 401, message: user.errors.full_messages.join(", ")}
+		end
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User
   field :fname, type: String
   field :lname, type: String
   field :email, type: String
+
+  validates :fname, :email, presence: true
+  validates :email, uniqueness: true
+  
 end
